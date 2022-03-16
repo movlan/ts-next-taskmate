2209752eddf5aaf98f13366041464819e185a6d5
Bahram Movlanov <bahram.movlanov@passportinc.com>
2022-03-15 18:43:10 -0600

    fix up

--- backend/schema.ts
+++ backend/schema.ts
@@ -1,6 +1,6 @@
 import { typeDefs } from "./type-defs";
 import { resolvers } from "./resolvers";
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 export const schema = makeExecutableSchema({
   typeDefs,
